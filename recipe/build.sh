175e514494742d910c499b4e17f167122e0b6a2b
Wes McKinney <wesm@users.noreply.github.com>
2017-04-19 21:33:38 -0400

    Update to 1.1.0pre. Pin Arrow runtime version. Make arrow-cpp conda package not required at build time (#17)
    * Update to 1.1.0pre, remove arrow-cpp from conda build dependencies. Pin Arrow version in build.sh
    
    * Re-render with conda-smithy 2.3.0
    
    * Remove ARROW_HOME
    
    * Update after PARQUET-959
    
    * Executables don't link properly with thirdparty Arrow in conda-forge
    
    * Set CMAKE_INSTALL_LIBDIR. Remove arrow-cpp from run requirements for now

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,11 @@ set -x
 
 # Build dependencies
 export BOOST_ROOT=$PREFIX
-export ARROW_HOME=$PREFIX
 export SNAPPY_HOME=$PREFIX
 export THRIFT_HOME=$PREFIX
 export ZLIB_HOME=$PREFIX
 export BROTLI_HOME=$PREFIX
+export PARQUET_ARROW_VERSION=0dc6fe8f33befaaa5fc8055b6c157ac1ccb09e6b
 
 mkdir build-dir
 cd build-dir
@@ -17,9 +17,11 @@ cd build-dir
 cmake \
     -DCMAKE_BUILD_TYPE=release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
     -DPARQUET_ARROW=on \
     -DPARQUET_BOOST_USE_SHARED=off \
     -DPARQUET_BUILD_BENCHMARKS=off \
+    -DPARQUET_BUILD_EXECUTABLES=off \
     -DPARQUET_ZLIB_VENDORED=off \
     -DPARQUET_BUILD_TESTS=off \
     ..

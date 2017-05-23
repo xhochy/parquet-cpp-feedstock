3bda2dab768544d6c32c8e0f0c936dbf328e270d
Wes McKinney <wesm@users.noreply.github.com>
2017-05-22 23:39:24 -0700

    Build Windows against pinned version of Arrow (#24)
    * Set Arrow version in bld.bat
    
    * Do not build tests in conda-forge

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,5 @@
 set PARQUET_BUILD_TOOLCHAIN=%LIBRARY_PREFIX%
+set PARQUET_ARROW_VERSION=a8f8ba0cbcf5f596f042e90b7a208e7a0c3925b7
 
 REM Set short build path to not overcome max obj files path length of 150 characters on Windows
 mkdir C:\bld\build
@@ -7,6 +8,7 @@ pushd C:\bld\build
 cmake -G "%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE=Release ^
                              -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
                              -DPARQUET_BOOST_USE_SHARED=OFF ^
+                             -DPARQUET_BUILD_TESTS=OFF ^
                              -DPARQUET_ZLIB_VENDORED=OFF ^
                              "%SRC_DIR%"
 

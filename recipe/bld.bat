43ff4f0dc8a6423cfa27270250d350913b86f022
Wes McKinney <wesm@users.noreply.github.com>
2017-06-12 10:00:34 -0400

    Update to HEAD, build against Arrow 0.4.1.  (#26)
    * Update to HEAD, build against Arrow 0.4.1. Remove arrow-cpp from build requirements
    
    * parquet_arrow artifacts no longer produced

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,5 @@
 set PARQUET_BUILD_TOOLCHAIN=%LIBRARY_PREFIX%
+set PARQUET_ARROW_VERSION=46315431aeda3b6968b3ac4c1087f6d41052b99d
 
 REM Set short build path to not overcome max obj files path length of 150 characters on Windows
 mkdir C:\bld\build

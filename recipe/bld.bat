35887e6f505c578b7d0f182c9bb527a82cb7d099
Wes McKinney <wesm@users.noreply.github.com>
2017-07-17 10:03:47 -0400

    1.2.0.pre update, remove now unneeded compression build dependencies (#28)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 set PARQUET_BUILD_TOOLCHAIN=%LIBRARY_PREFIX%
-set PARQUET_ARROW_VERSION=46315431aeda3b6968b3ac4c1087f6d41052b99d
+set PARQUET_ARROW_VERSION=5fbfd8e58392439c754cdf19f6f3b27b28303e0a
 
 REM Set short build path to not overcome max obj files path length of 150 characters on Windows
 mkdir C:\bld\build
@@ -9,7 +9,6 @@ cmake -G "%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE=Release ^
                              -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
                              -DPARQUET_BOOST_USE_SHARED=OFF ^
                              -DPARQUET_BUILD_TESTS=OFF ^
-                             -DPARQUET_ZLIB_VENDORED=OFF ^
                              "%SRC_DIR%"
 
 cmake --build . --target install --config Release
